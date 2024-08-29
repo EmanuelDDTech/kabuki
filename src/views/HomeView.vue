e7642da99aa3854891c513acc4bddc41f4d35b68
EmanuelDDTech <emanuel@ddtech.mx>
2024-08-29 16:03:12 -0600

    Inicialización del proyecto tienda Pokémon

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Kabuki Cards</h1>
+  </main>
+</template>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Vista de productos</h1>
+  </div>
+</template>
