5399948c1246f58971aaaaec34a3b2c8fef2d1d6
EmanuelDDTech <emanuel@ddtech.mx>
2024-08-31 08:00:25 -0600

    Componente del header general.

--- src/modules/layouts/components/GeneralHeader.vue
+++ src/modules/layouts/components/GeneralHeader.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <h2>Header</h2>
-  </div>
+  <header class="border-b">
+    <div class="flex justify-between items-center max-w-7xl mx-auto py-3">
+      <div class="w-40"><img src="@assets/img/logo.avif" alt=""></div>
+      <SearchBar />
+      <nav class="flex gap-4 text-base">
+        <RouterLink :to="{name: 'home'}" class="flex items-center">Registrarse</RouterLink>
+        <RouterLink :to="{name: 'home'}" ><CartIcon /></RouterLink>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts">
+import SearchBar from '@/modules/search/components/SearchBar.vue';
+import CartIcon from '@cart/components/CartIcon.vue';
+import { RouterLink } from 'vue-router';
+
 
 </script>
 
