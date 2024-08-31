77fc68d30b7d85ed6671ff928e1e070ead55997b
EmanuelDDTech <emanuel@ddtech.mx>
2024-08-31 08:02:26 -0600

    Layout general del sitio web.

--- src/modules/layouts/pages/GeneralLayout.vue
+++ src/modules/layouts/pages/GeneralLayout.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <GeneralHeader />
-    <RouterView />
-    <GeneralFooter />
+    <GeneralHeader class="px-3" />
+    <NavBar :routes="routes" class="px-3" />
+    <MainBanner />
+    <RouterView class="px-3" />
+    <GeneralFooter class="px-3" />
   </div>
 </template>
 
@@ -10,6 +12,13 @@
 import { RouterView } from 'vue-router'
 import GeneralHeader from '../components/GeneralHeader.vue';
 import GeneralFooter from '../components/GeneralFooter.vue';
+import NavBar from '../components/NavBar.vue';
+import MainBanner from '../components/MainBanner.vue';
+
+const routes = [
+  { name: 'home', text: 'Inicio' },
+  { name: 'products', text: 'Productos' }
+]
 </script>
 
 <style scoped>
