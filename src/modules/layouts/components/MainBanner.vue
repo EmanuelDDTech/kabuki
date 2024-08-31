20c825eaaff35ecf9a6e8b96eb0913bb28713912
EmanuelDDTech <emanuel@ddtech.mx>
2024-08-31 08:01:33 -0600

    Componente para el banner principal.

---
+++ src/modules/layouts/components/MainBanner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-[600px] w-full bg-center bg-cover" style="background-image: url('./src/assets/img/banner.jpg');">
+    <!-- <img src="@/assets/img/banner.jpg" alt=""> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
