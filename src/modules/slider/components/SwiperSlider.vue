c91a8ad0e2e4becc32ad1a2ef8753a5f26df8f64
EmanuelDDTech <emanuel@ddtech.mx>
2024-09-02 23:49:20 -0600

    Componente para sliders de productos.

---
+++ src/modules/slider/components/SwiperSlider.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+  <header class="flex mb-6">
+    <h2 class="text-3xl font-bold">Preventa</h2>
+    <SwiperNavButtons />
+  </header>
+  
+  <Swiper
+    :modules="modules"
+    :spaceBetween="20"
+    :slidesPerView="5"
+    :navigation="{ nextEl: '#swiper-next', prevEl: '#swiper-prev'}"
+    className="py-3 overflow-hidden"
+  >
+    <SwiperSlide> 
+      <ProductCard />
+    </SwiperSlide>
+    <SwiperSlide> 
+      <ProductCard />
+    </SwiperSlide>
+    <SwiperSlide> 
+      <ProductCard />
+    </SwiperSlide>
+    <SwiperSlide> 
+      <ProductCard />
+    </SwiperSlide>
+    <SwiperSlide> 
+      <ProductCard />
+    </SwiperSlide>
+    <SwiperSlide> 
+      <ProductCard />
+    </SwiperSlide>
+    <SwiperSlide> 
+      <ProductCard />
+    </SwiperSlide>
+  </Swiper>
+</div>
+</template>
+
+<script setup lang="ts">
+import { Navigation, Pagination} from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import SwiperNavButtons from './SwiperNavButtons.vue'
+import ProductCard from '@/modules/products/components/ProductCard.vue'
+
+const modules = [Pagination, Navigation]
+
+// interface Props {
+
+// }
+
+// defineProps({
+//   products: {
+//     type: Array,
+//     required: true,
+//   }
+// })
+
+</script>
