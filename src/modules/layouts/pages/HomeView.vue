fe655f9704887a7b16a4222b5051dc318b65a330
EmanuelDDTech <emanuel@ddtech.mx>
2024-09-02 23:51:57 -0600

    Refactor del slider con swiper.

--- src/modules/layouts/pages/HomeView.vue
+++ src/modules/layouts/pages/HomeView.vue
@@ -2,35 +2,14 @@
 import ArrowLeft from '@/modules/icons/ArrowLeft.vue';
 import ArrowRight from '@/modules/icons/ArrowRight.vue';
 import ProductCard from '@/modules/products/components/ProductCard.vue';
+import SwiperSlider from '@/modules/slider/components/SwiperSlider.vue';
 </script>
 
 <template>
   <main>
     <section class="bg-gray-100 px-3">
       <div class="max-w-screen-xl mx-auto py-14">
-        <header class="flex">
-          <h2 class="text-3xl font-bold">Preventa</h2>
-          
-          <div class="flex-1 flex justify-end gap-3 text-gray-500">
-            <ArrowLeft class="w-11 h-8 p-1 border-2 border-gray-500 rounded cursor-pointer hover:bg-black hover:border-black hover:text-white transition-colors"/>
-            <ArrowRight class="w-11 h-8 p-1 border-2 border-gray-500 rounded cursor-pointer hover:bg-black hover:border-black hover:text-white transition-colors"/>
-          </div>
-        </header>
-
-        <div class="flex gap-3 mt-6">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-        </div>
+        <SwiperSlider />
       </div>
     </section>
     <section class="px-3">
