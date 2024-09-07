c7a5eb7441c99977e1fd8f6e6c2baa72c01cb57d
EmanuelDDTech <emanuel@ddtech.mx>
2024-09-06 21:26:47 -0600

    Refactor CountdownTimer.

--- src/modules/counter/components/CountdownTimer.vue
+++ src/modules/counter/components/CountdownTimer.vue
@@ -2,24 +2,31 @@
   <div class="circle-container">
     <div class="circle text-xl flex items-center gap-2">
       <p
-        :class="{ 'clock-stopped': !isStarted }"
+        :class="{ 'clock-stopped': stopTimer }"
         class="text-white bg-red-500 p-1 rounded-full w-9 text-center aspect-square"
       >
-        {{ hourString }}
+        {{ dayString > 0 ? dayString : '00' }}
       </p>
       <span>:</span>
       <p
-        :class="{ 'clock-stopped': !isStarted }"
+        :class="{ 'clock-stopped': stopTimer }"
         class="text-white bg-red-500 p-1 rounded-full w-9 text-center aspect-square"
       >
-        {{ minuteString }}
+        {{ hourString > 0 ? hourString : '00' }}
       </p>
       <span>:</span>
       <p
-        :class="{ 'clock-stopped': !isStarted }"
+        :class="{ 'clock-stopped': stopTimer }"
         class="text-white bg-red-500 p-1 rounded-full w-9 text-center aspect-square"
       >
-        {{ secondString }}
+        {{ minuteString > 0 ? minuteString : '00' }}
+      </p>
+      <span>:</span>
+      <p
+        :class="{ 'clock-stopped': stopTimer }"
+        class="text-white bg-red-500 p-1 rounded-full w-9 text-center aspect-square"
+      >
+        {{ secondString > 0 ? secondString : '00' }}
       </p>
     </div>
   </div>
@@ -30,66 +37,51 @@ import { ref, computed, watch, onMounted } from 'vue';
 
 const emit = defineEmits(['timeDecrease']);
 const clock = ref('00:00:00');
+const dayString = ref<string | number>('');
 const hourString = ref<string | number>('');
 const minuteString = ref<string | number>('');
 const secondString = ref<string | number>('');
+const stopTimer = ref<boolean>(false);
+
 const props = defineProps<{
   duration: number;
-  isStarted: boolean;
+  finishDate?: date;
 }>();
+
 let intervalId: number;
-// let soundOn = ref(true);
 
-const isStarted = computed(() => {
-  return props.isStarted;
-});
-
-const duration = computed(() => {
-  return props.duration;
-});
-
-let storedDuration = props.duration;
+let storedDuration = props.finishDate - new Date();
 
 onMounted(() => {
-  if (isStarted.value) {
-    startTimer();
-  } else {
-    stopTimer();
-  }
+  startTimer();
 });
 
 const startTimer = function () {
   intervalId = setInterval(() => {
     if (storedDuration <= 0) {
+      stopTimer.value = true;
       clearInterval(intervalId);
     } else {
-      updateDisplay(storedDuration - 1);
-      storedDuration--;
+      updateDisplay(storedDuration - 1000);
+      storedDuration = storedDuration - 1000;
       emit('timeDecrease');
     }
   }, 1000);
 };
 
-const stopTimer = function () {
-  clearInterval(intervalId);
-};
-
 function updateDisplay(time: number) {
-  let hours, minutes, seconds: number;
+  let days, hours, minutes, seconds: number;
 
-  hours = Math.floor(time / 60 / 60);
-  minutes = Math.floor(time / 60 - hours * 60);
-  seconds = Math.floor(time % 60);
+  days = Math.floor(time / 86400000);
+  hours = Math.floor((time / 3600000) % 24);
+  minutes = Math.floor((time / 60000) % 60);
+  seconds = Math.floor((time / 1000) % 60);
 
+  dayString.value = days < 10 ? '0' + days : days;
   hourString.value = hours < 10 ? '0' + hours : hours;
   minuteString.value = minutes < 10 ? '0' + minutes : minutes;
   secondString.value = seconds < 10 ? '0' + seconds : seconds;
 
-  clock.value = `${hourString.value}:${minuteString.value}:${secondString.value}`;
+  clock.value = `${dayString.value}:${hourString.value}:${minuteString.value}:${secondString.value}`;
 }
-
-watch(duration, () => {
-  storedDuration = duration.value;
-  updateDisplay(storedDuration);
-});
 </script>
