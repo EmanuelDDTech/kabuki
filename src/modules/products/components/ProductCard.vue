c6c1b9d65e0d32f8d1680b1a5f87926e2d141d80
EmanuelDDTech <emanuel@ddtech.mx>
2024-09-10 23:01:44 -0600

    Separate wishIcon into a component and redirect to product view on click.

--- src/modules/products/components/ProductCard.vue
+++ src/modules/products/components/ProductCard.vue
@@ -1,6 +1,7 @@
 <template>
   <article
     class="relative max-w-72 min-w-56 bg-white shadow-md rounded-3xl p-2 cursor-pointer border border-gray-100"
+    @click="router.push({ name: 'product', params: { id: 12345 } })"
   >
     <div
       class="aspect-[4/3] overflow-x-hidden rounded-2xl relative flex items-center justify-center"
@@ -9,10 +10,10 @@
       <p class="absolute right-2 top-2 rounded-full p-2 cursor-pointer group bg-gray-100">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 group-hover:opacity-50 opacity-70"
+          class="h-6 aspect-square group-hover:opacity-70 text-gray-500"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="black"
+          stroke="currentColor"
         >
           <path
             stroke-linecap="round"
@@ -29,26 +30,18 @@
         <p class="text-xl text-black font-bold mt-0">$300</p>
         <p class="text-base text-gray-500 line-through font-normal mt-0">$340</p>
         <div class="group cursor-pointer bg-gray-100 p-2 rounded-full absolute right-2 bottom-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 group-hover:opacity-70"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="gray"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            />
-          </svg>
+          <WishlistIcon class="h-6 aspect-square text-gray-500 group-hover:opacity-70" />
         </div>
       </div>
     </div>
   </article>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import WishlistIcon from '@/modules/cart/components/wishlistIcon.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
 
 <style scoped></style>
