b35737834bb2906d5313c9b4ad428fbaca0fb873
EmanuelDDTech <emanuel@ddtech.mx>
2024-10-27 10:24:53 -0600

    Prettier modifications to AdminLink.vue

--- src/modules/admin/components/AdminLink.vue
+++ src/modules/admin/components/AdminLink.vue
@@ -1,8 +1,7 @@
-<script setup>
-import { ref } from 'vue';
+<script lang="ts" setup>
 import { RouterLink } from 'vue-router';
 
-const props = defineProps({
+defineProps({
   route: {
     type: String,
     required: true,
@@ -15,13 +14,15 @@ const props = defineProps({
     type: Object,
     required: true,
   },
-})
-
+});
 </script>
 
 <template>
-<RouterLink :to="{ name: route }" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-  <component :is="icon" />
-  <span class="ml-3 flex-1 whitespace-nowrap">{{ text }}</span>
-</RouterLink>
+  <RouterLink
+    :to="{ name: route }"
+    class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group"
+  >
+    <component :is="icon" />
+    <span class="ml-3 flex-1 whitespace-nowrap">{{ text }}</span>
+  </RouterLink>
 </template>
