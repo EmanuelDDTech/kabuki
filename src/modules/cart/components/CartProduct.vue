434d551c61210ef7b5e1ff1acb77e16b8e3019e7
EmanuelDDTech <emanuel@ddtech.mx>
2025-01-10 20:32:17 -0600

    feat(carrito): add sum and res functions

--- src/modules/cart/components/CartProduct.vue
+++ src/modules/cart/components/CartProduct.vue
@@ -13,7 +13,11 @@
 
       <div class="mt-2 flex gap-3 items-center">
         <p class="font-bold">Cantidad:</p>
-        <ProductQuantity :value="item.quantity" />
+        <ProductQuantity
+          :value="item.quantity"
+          @res="res(item.product.id, item.quantity, item.product.stock)"
+          @sum="sum(item.product.id, item.quantity, item.product.stock)"
+        />
       </div>
 
       <div class="flex text-sm gap-6 mt-2 text-gray-500">
@@ -28,14 +32,65 @@
 </template>
 
 <script setup lang="ts">
+import { inject } from 'vue';
 import ProductQuantity from '@/modules/counter/components/ProductQuantity.vue';
 import { formatCurrency } from '@/helpers';
+import { useCartStore } from '@/modules/cart/stores/cart';
+
+const cart = useCartStore();
+const toast = inject('toast');
 
 interface Props {
   item?: Object;
 }
 
 const props = defineProps<Props>();
+
+const res = async (productId, quantity) => {
+  if (quantity <= 0) {
+    toast.open({
+      message: 'La cantidad ya está en 0',
+      type: 'error',
+    });
+    return;
+  }
+
+  try {
+    await cart.reduceQuantity(productId, quantity);
+    toast.open({
+      message: 'Cantidad actualizada correctamente',
+      type: 'success',
+    });
+  } catch (error) {
+    toast.open({
+      message: error.response.data.msg,
+      type: 'error',
+    });
+  }
+};
+
+const sum = async (productId, quantity, stock) => {
+  if (quantity >= stock) {
+    toast.open({
+      message: 'No hay más stock disponible',
+      type: 'error',
+    });
+    return;
+  }
+
+  try {
+    await cart.increaseQuantity(productId, quantity);
+    toast.open({
+      message: 'Cantidad actualizada correctamente',
+      type: 'success',
+    });
+  } catch (error) {
+    toast.open({
+      message: error.response.data.msg,
+      type: 'error',
+    });
+  }
+};
 </script>
 
 <style scoped></style>
