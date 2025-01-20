95d7289e3000d03988a350ca289a1f382cec3ff5
EmanuelDDTech <emanuel@ddtech.mx>
2025-01-19 23:14:16 -0600

    feat(address): add address form

--- src/modules/cart/pages/CartView.vue
+++ src/modules/cart/pages/CartView.vue
@@ -1,32 +1,270 @@
 <template>
-  <main class="min-h-screen px-3">
+  <main class="min-h-screen px-3 mb-8">
     <div class="max-w-screen-xl mx-auto flex gap-10 mt-8">
-      <div class="flex-1 shadow-md border border-gray-100 p-4 rounded-lg">
+      <div class="flex-1">
+        <section class="flex-1 shadow-md border border-gray-100 p-4 rounded-lg">
         <h2 class="text-2xl font-bold border-b-2 border-gray-200 pb-3">Carrito</h2>
 
         <CartProduct v-for="item in cart.items" :key="item.product.id" :item="item" />
+        </section>
+
+        <section class="mt-6 shadow-md border border-gray-100 p-4 rounded-lg">
+          <h2 class="text-2xl font-bold border-b-2 border-gray-200 pb-3 mb-6">
+            Dirección de entrega
+          </h2>
+
+          <div
+            v-if="address.createNew"
+            @click="goBack()"
+            class="flex gap-2 items-center justify-start mb-4 text-red-700 font-medium rounded-lg text-sm cursor-pointer"
+          >
+            <LeftArrow class="w-6" />
+            <p>Volver</p>
+          </div>
+
+          <div v-if="address.createNew">
+            <FormKit
+              id="newAddressForm"
+              type="form"
+              @submit="saveAddress()"
+              :actions="false"
+              class="max-w-lg mx-auto p-4 space-y-4 bg-white rounded shadow"
+            >
+              <!-- País / Región -->
+              <FormKit
+                type="select"
+                name="country"
+                v-model="address.newAddress.country"
+                label="País / Región"
+                :options="['México', 'Estados Unidos']"
+                validation="required"
+                placeholder="Selecciona un país"
+              />
+
+              <!-- Dirección -->
+              <FormKit
+                type="text"
+                name="street"
+                v-model="address.newAddress.street"
+                label="Dirección"
+                validation="required"
+                placeholder="Ingresa tu dirección"
+              />
+
+              <!-- Colonia -->
+              <FormKit
+                type="text"
+                name="colony"
+                v-model="address.newAddress.colony"
+                label="Colonia"
+                validation="required"
+                placeholder="Ingresa tu colonia"
+              />
+
+              <!-- Código Postal, Ciudad, Estado -->
+              <div class="grid grid-cols-3 gap-4">
+                <FormKit
+                  type="text"
+                  name="zip"
+                  v-model="address.newAddress.zip"
+                  label="Código Postal"
+                  validation="required|matches:/^\d{5}$/"
+                  placeholder="XXXXX"
+                  :validation-messages="{
+                    required: 'El código postal es obligatorio',
+                    matches: 'Ingresa un código válido',
+                  }"
+                />
+                <FormKit
+                  type="text"
+                  name="city"
+                  v-model="address.newAddress.city"
+                  label="Ciudad"
+                  validation="required"
+                  placeholder="Ingresa tu ciudad"
+                />
+                <FormKit
+                  type="select"
+                  name="state"
+                  v-model="address.newAddress.state"
+                  label="Estado"
+                  :options="['Jalisco', 'CDMX', 'Nuevo León']"
+                  validation="required"
+                  placeholder="Selecciona un estado"
+                />
+              </div>
+
+              <!-- Teléfono -->
+              <FormKit
+                type="tel"
+                name="phone"
+                v-model="address.newAddress.phone"
+                label="Teléfono"
+                validation="required|matches:/^\d{10}$/"
+                placeholder="Ingresa tu teléfono"
+              />
+
+              <!-- Checkbox -->
+              <!-- <FormKit
+                type="checkbox"
+                name="saveInfo"
+                label="Guardar la información de envio"
+                wrapper-class="flex items-center gap-2"
+                label-class="!mt-0"
+              /> -->
+
+              <!-- Botón de envío -->
+              <FormKit
+                type="submit"
+                label="Confirmar Dirección"
+                button-class="w-full py-2 bg-blue-600 text-white rounded hover:bg-blue-700 mt-3"
+              />
+            </FormKit>
+
+            <button
+              v-if="address.newAddress.id !== 0"
+              class="text-red-500 mt-3"
+              @click.stop="deleteConfirmation(address.newAddress.id)"
+            >
+              Eliminar dirección
+            </button>
+          </div>
+
+          <div v-if="!address.createNew" class="mb-3 flex flex-col gap-3">
+            <AddressCard
+              v-for="addressData in address.savedAddresses"
+              :key="addressData.id"
+              :address="addressData"
+              :selected="address.isSelected(addressData.id)"
+              @click="address.selectAddress(addressData.id)"
+              @edit="address.editAddress(addressData)"
+            />
+          </div>
+
+          <div v-if="!address.createNew">
+            <button
+              class="bg-blue-500 hover:bg-blue-600 rounded-lg text-white font-bold w-full p-3 mt-3"
+              @click="address.setCreateNew(true)"
+            >
+              Agregar nueva dirección
+            </button>
+          </div>
+        </section>
       </div>
-      <aside class="w-72 h-fit shadow-md border border-gray-100 p-4 rounded-lg">
+
+      <div>
+        <aside class="h-full">
+          <div class="w-72 shadow-md border border-gray-100 p-4 rounded-lg sticky top-3">
         <h3 class="text-xl">
           Subtotal: <span class="font-bold">{{ formatCurrency(cart.total) }}</span>
         </h3>
-        <RouterLink
-          :to="{ name: 'pay' }"
+            <button
+              @click="cart.checkout()"
           class="block text-center w-full rounded-full bg-yellow-300 hover:bg-yellow-400 py-1 mt-3 text-sm transition-colors"
-          >Pagar</RouterLink
-        >
+            >
+              Pagar
+            </button>
+          </div>
       </aside>
+      </div>
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
+import { inject, onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
 import CartProduct from '../components/CartProduct.vue';
 import { useCartStore } from '../stores/cart';
 import { formatCurrency } from '@/helpers';
+import { FormKit, reset } from '@formkit/vue';
+// import { AddressValidationClient } from '@googlemaps/addressvalidation';
+import { useAddressStore } from '../stores/address';
+import LeftArrow from '@/modules/icons/ArrowLeft.vue';
+import AddressCard from '../components/AddressCard.vue';
+import Swal from 'sweetalert2';
 
 const cart = useCartStore();
+const address = useAddressStore();
+// const addressValidationClient = new AddressValidationClient({
+//   keyFilename: '/credential/google-credentials.json',
+// });
+
+const toast = inject('toast');
+
+onMounted(() => {
+  address.getAddresses();
+
+});
+
+const saveAddress = async () => {
+  try {
+    await address.saveAddress();
+
+    reset('newAddressForm');
+
+    const message =
+      address.newAddress.id === 0
+        ? 'Dirección creada correctamente'
+        : 'Dirección actualizada correctamente';
+    address.clearNewAddress();
+
+    toast.open({
+      message: message,
+      type: 'success',
+    });
+
+    setTimeout(() => {
+      address.createNew = false;
+    }, 1000);
+  } catch (error) {
+    toast.open({
+      message: error.response.data.msg,
+      type: 'error',
+    });
+  }
+};
+
+const goBack = () => {
+  address.setCreateNew(false);
+  address.clearNewAddress();
+};
+
+const deleteConfirmation = (id: number) => {
+  Swal.fire({
+    title: 'Seguro quieres eliminar esta dirección?',
+    text: 'No podrá ser revertido!',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si, Eliminar!',
+    cancelButtonText: 'Cancelar',
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      await deleteAddress(id);
+    }
+  });
+};
+
+const deleteAddress = async (id: number) => {
+  try {
+    await address.deleteAddress(id);
+    toast.open({
+      message: 'Dirección eliminada correctamente',
+      type: 'success',
+    });
+
+    setTimeout(() => {
+      address.createNew = false;
+    }, 1000);
+  } catch (error) {
+    toast.open({
+      message: error.response.data.msg,
+      type: 'error',
+    });
+  }
+};
 </script>
 
 <style scoped></style>
