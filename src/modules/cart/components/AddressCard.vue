38fa756a045b8f23aa0e2d6ccf6e9cd1b8c8f679
EmanuelDDTech <emanuel@ddtech.mx>
2025-01-19 23:11:15 -0600

    feat(address): add address card

---
+++ src/modules/cart/components/AddressCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <article
+    class="shadow hover:shadow-blue-600 relative p-3 rounded border hover:border-blue-500 cursor-pointer"
+    :class="selected ? 'shadow-blue-600 border-blue-500' : 'border-gray-100'"
+  >
+    <h3 class="font-bold text-xl mb-3">Enviar a:</h3>
+
+    <div>
+      <p class="text-gray-600">
+        {{
+          `${address.street}, ${address.colony}, ${address.city}, ${address.state}, ${address.zip}, ${address.country}`
+        }}
+      </p>
+    </div>
+
+    <EditIcon
+      class="w-7 absolute top-3 right-3 cursor-pointer hover:text-blue-600"
+      @click="$emit('edit')"
+    />
+  </article>
+</template>
+
+<script lang="ts" setup>
+import EditIcon from '@/modules/common/icons/EditIcon.vue';
+import type { Address } from '../interfaces/address.interface';
+
+interface Props {
+  address: Address;
+  selected: Boolean;
+}
+
+defineProps<Props>();
+</script>
