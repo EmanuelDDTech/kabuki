6c92830270efc8848849c16ae088a266a521cd53
EmanuelDDTech <emanuel@ddtech.mx>
2025-02-04 00:16:27 -0600

    feat(campaign): add CampaignAPI

---
+++ src/modules/campaign/api/CampaignAPI.ts
@@ -0,0 +1,19 @@
+import api from '@/lib/axios';
+
+export default {
+  async create(data: object) {
+    return await api.post('/campanas', data);
+  },
+  async getAll() {
+    return await api.get('/campanas');
+  },
+  async getById(id: number) {
+    return await api.get(`/campanas/${id}`);
+  },
+  async update(id: number, data: object) {
+    return await api.put(`/campanas/${id}`, data);
+  },
+  async delete(id: number) {
+    return await api.delete(`/campanas/${id}`);
+  },
+};
