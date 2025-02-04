792a772a255269fb97e5e284a22cbd76f55b0091
EmanuelDDTech <emanuel@ddtech.mx>
2025-02-04 00:19:42 -0600

    feat(campaign): add UpdateCampaign view

---
+++ src/modules/campaign/pages/UpdateCampaignView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="mt-6 mx-4 p-4 sm:p-6 xl:p-8 bg-white shadow rounded-lg">
+    <CampaignForm :id="id" />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+import CampaignForm from '../components/CampaignForm.vue';
+
+const route = useRoute();
+const id: number = parseInt(route.params.id);
+</script>
