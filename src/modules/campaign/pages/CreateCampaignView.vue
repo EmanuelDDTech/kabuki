40ae4b410bcd3ad55012026b411e7ec1b382e9fd
EmanuelDDTech <emanuel@ddtech.mx>
2025-02-04 00:19:25 -0600

    feat(campaign): add CreateCampaign view

---
+++ src/modules/campaign/pages/CreateCampaignView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import CampaignForm from '../components/CampaignForm.vue';
+</script>
+
+<template>
+  <main class="mt-6 mx-4 p-4 sm:p-6 xl:p-8 bg-white shadow rounded-lg">
+    <CampaignForm />
+  </main>
+</template>
