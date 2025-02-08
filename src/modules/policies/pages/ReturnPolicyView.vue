4cd3e281e68a73a685285ac5c634469b35919bf8
EmanuelDDTech <emanuel@ddtech.mx>
2025-02-08 06:41:47 -0600

    feat(policies): add policies pages

---
+++ src/modules/policies/pages/ReturnPolicyView.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="min-h-screen justify-center bg-white p-4">
+    <div class="bg-white rounded-lg p-6 w-full">
+      <h1 class="text-2xl font-semibold text-gray-800 mb-4">Política de Devolución</h1>
+      <div class="h-64 rounded-md text-gray-600 text-sm">
+        <p>
+          Los productos coleccionables tienden a tener muchas restricciones para ser devueltos por
+          lo tanto, No manejamos devoluciones ni retornos para nuestros productos, pero si hay algun
+          problema con tu producto o envío puedes ponerte en contacto con nosotros al siguiente
+          correo: y veremos de que manera poder resolverlo.
+        </p>
+        <p class="mt-2">
+          Shorikamecards es una Tienda de productos limitados y demandado por lo que todas nuestras
+          ventas son finales, no tenemos cambios ni reembolsos; a menos que tengamos una situación
+          extraordinaria podremos cambiar y/o rembolsar en los siguientes casos:
+        </p>
+
+        <p class="mt-2">
+          Debido a la alta rotación y demanda de nuestros productos es importante para nosotros
+          asegurarnos de la existencia y condiciones óptimas de tu compra, por lo que una vez
+          recibido el pago de tu pedido contaremos con un lapso máximo de 72 hrs. para confirmar si
+          tu pedido:
+        </p>
+        <ol class="mt-2 list-decimal pl-4">
+          <li class="mt-1">Se enviará completo.</li>
+          <li class="mt-1">Se enviará parcial y se reembolsará el producto faltante.</li>
+          <li class="mt-1">Se cancelará y se te reembolsará el costo total pagado.</li>
+        </ol>
+
+        <p class="mt-2">
+          Al realizar tu pedido (pago realizado), no podrás cancelarlo, por lo que deberás esperar a
+          recibir el pedido.
+        </p>
+        <p class="mt-2">
+          No se podrán realizar cambios de modelo, talla o colorway, por lo que es importante que
+          antes de confirmar tu compra, te cerciores de comprar el producto de tu elección.
+        </p>
+
+        <p class="mt-2">
+          Podemos realizar cancelaciones y/o devoluciones, sólo en los siguientes casos:
+        </p>
+        <ol class="mt-2 list-decimal pl-4">
+          <li class="mt-1">
+            Error de Proceso, en el cual recibas la talla incorrecta o diferente al pedido que
+            solicitaste o bien un producto que no solicitaste en tu orden.
+          </li>
+        </ol>
+
+        <p class="mt-2">
+          Bajo ningun motivo podremos realizar la devolución en los siguientes casos:
+        </p>
+        <ol class="mt-2 list-decimal pl-4">
+          <li class="mt-1">Producto usado/abierto</li>
+          <li class="mt-1">Producto maltratado</li>
+          <li class="mt-1">Productos de edición limitada o lanzamientos</li>
+        </ol>
+
+        <h2 class="mt-4 text-xl font-bold">Metodo de entrega:</h2>
+        <p class="mt-2">
+          Envío por paquetería: Manejamos un tiempo estimado de 2-3 días hábiles, en ocasiones
+          podrás recibirlo al día siguiente de tu compra; y tenemos un tiempo máximo estimado de 8
+          días hábiles; esto por situaciones extraordinarias que la paquetería pudiera presentar y
+          que lamentablemente no podemos controlar
+        </p>
+      </div>
+      <!-- <div class="mt-4 flex justify-end">
+        <button
+          @click="acceptTerms"
+          class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+        >
+          Acepto
+        </button>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// function acceptTerms() {
+//   alert('Has aceptado los términos y condiciones.');
+// }
+</script>
---
+++ src/modules/policies/pages/TermsAndQuestionsView.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="bg-white p-4">
+    <div class="bg-white rounded-lg p-6 w-full">
+      <h1 class="text-2xl font-semibold text-gray-800 mb-4">
+        Términos y Condiciones y Preguntas Frecuentes
+      </h1>
+      <div class="rounded-md text-gray-600 text-sm">
+        <h2 class="mt-4 text-xl font-bold">¿Cuál es el costo de envío?</h2>
+        <p class="mt-2">
+          El costo de envío será mostrado en base al total de la compra y ubicación, en el checkout,
+          en el momento previo a la compra.
+        </p>
+
+        <h2 class="mt-4 text-xl font-bold">¿Como funcionan las preventas?</h2>
+        <p class="mt-2">
+          Las preventas las podras comprar directamente en nuestra pagina solamente con el anticipo,
+          al llegarse el dia de liquidar las preventas tal como lo mencionemos en cada publicacion,
+          podran liquidar por medio de LINK DE PAGO o TRANSFERENCIA. Nosotros hacemos llegar un
+          mensaje a todos con un poco de tiempo antes de la fecha limite. No te confies y ponlo en
+          tu calendario, puede que si la expansion es muy demandada el mensaje recordatorio te
+          llegue muy cercas a la fecha limite.
+        </p>
+        <p class="mt-2">LAS PRE ORDENES NO TIENEN ENVIO GRATIS.</p>
+        <p class="mt-2">
+          IMPORTANTE: QUIENES NO PAGUEN SUS PRE ORDENES AL TIEMPO ESTABLECIDO, PERDERAN EL ANTICIPO
+          Y EL DERECHO A OBTENER LA PREVENTA EN EL LANZAMIENTO, YA QUE MANEJAMOS DIFERENTES PRECIOS
+          PARA PREVENTAS QUE PARA VENTA AL PUBLICO GENERAL.
+        </p>
+
+        <h2 class="mt-4 text-xl font-bold">¿Cómo se realizan los envíos o entregas locales?</h2>
+        <p class="mt-2">
+          Shorikamecards trabaja solamente con DHL, PAQUETEXPRESS, ESTAFETA Y FEDEX.
+        </p>
+        <p class="mt-2">
+          Si tu decides hacer la compra de un envio por medio de otra paqueteria o por skydrop por
+          medio de la pagina, no nos hacemos responsables por el seguro que no está incluido.
+        </p>
+        <p class="mt-2">Si deseas seguro para tu compra, puedes contactarnos via whatsapp.</p>
+
+        <h2 class="mt-4 text-xl font-bold">¿Dónde puedo recibir mi pedido?</h2>
+        <p class="mt-2">Realizamos envíos a todo el país.</p>
+
+        <h2 class="mt-4 text-xl font-bold">¿Cuánto tarda en llegar el pedido?</h2>
+        <p class="mt-2">
+          El tiempo de entrega dependerá del tipo de envío seleccionado. En general la demora se
+          encuentra entre 2 y 7 días hábiles luego de acreditado el pago.
+        </p>
+        <p class="mt-2">
+          Te recodamos que nosotros demoramos 1-3 dias hábiles en enviar tu producto, ya que solo
+          hacemos envios 3 veces a la semana. Si despues de 10 dias habiles no recibes tu pedido, te
+          haremos un reembolso o reenviaremos el paquete sin costo adicional.
+        </p>
+
+        <h2 class="mt-4 text-xl font-bold">¿Cuál es el plazo para realizar un cambio?</h2>
+        <p class="mt-2">No hacemos cambios ni devoluciones.</p>
+
+        <h2 class="mt-4 text-xl font-bold">
+          ¿Qué debo hacer si el producto no llega en buen estado?
+        </h2>
+        <p class="mt-2">
+          Ponte en contacto con nosotros a "correo" y veremos de que manera resolverlo.
+        </p>
+
+        <h2 class="mt-4 text-xl font-bold">¿Que tipo de garantías ofrecen?</h2>
+        <p class="mt-2">
+          Manejamos la garantía Shorikamecards, en donde tu producto es 100% sellado de fabrica y
+          nuevo. No arriesgues dinero comprando producto resellado
+        </p>
+
+        <h2 class="mt-4 text-xl font-bold">¿Facturan?</h2>
+        <p class="mt-2">
+          Si facturamos, nuestros productos no incluyen IVA, de momento todo lo que quisieras
+          facturar seria el precio mas iva del 8%.
+        </p>
+
+        <h2 class="mt-4 text-xl font-bold">
+          ¿Qué formas de pago puedo aprovechar para realizar mi compra?
+        </h2>
+        <p class="mt-2">POR DEFINIR</p>
+      </div>
+      <!-- <div class="mt-4 flex justify-end">
+        <button
+          @click="acceptTerms"
+          class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+        >
+          Acepto
+        </button>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// function acceptTerms() {
+//   alert('Has aceptado los términos y condiciones.');
+// }
+</script>
