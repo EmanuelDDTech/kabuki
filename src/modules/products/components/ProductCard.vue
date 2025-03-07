278d4044ad2fe8f36cadee566a86a49f7e31cb3d
EmanuelDDTech <emanuel@ddtech.mx>
2025-03-07 10:00:52 -0600

    feat(discount): Add discount

--- src/modules/products/components/ProductCard.vue
+++ src/modules/products/components/ProductCard.vue
@@ -47,8 +47,8 @@
         <p class="text-xl text-black font-bold mt-0">
           {{ formatCurrency(product.discount ? product.discount : product.price) }}
         </p>
-        <p class="text-base text-gray-500 line-through font-normal mt-0">
-          {{ formatCurrency(150) }}
+        <p v-if="product.discount" class="text-base text-gray-500 line-through font-normal mt-0">
+          {{ formatCurrency(product.price) }}
         </p>
         <div class="group cursor-pointer bg-gray-100 p-2 rounded-full absolute right-2 bottom-0">
           <WishlistIcon class="h-6 aspect-square text-gray-500 group-hover:opacity-70" />
@@ -70,7 +70,7 @@ const cart = useCartStore();
 const toast = inject('toast');
 
 interface Props {
-  product?: object;
+  product: object;
 }
 
 const props = defineProps<Props>();
