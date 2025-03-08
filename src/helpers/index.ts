ddf91009b7bb708d06df69a331cb07b718286920
EmanuelDDTech <emanuel@ddtech.mx>
2025-03-07 22:24:28 -0600

    feat(helpers): Change js to ts file

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
-export const formatCurrency = (price) => {
+export const formatCurrency = (price: Number) => {
   return Number(price).toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
-  })
-} 
+  });
+};
