d0b085c379e99f889f353e170afb1353d25ad098
EmanuelDDTech <emanuel@ddtech.mx>
2025-03-11 21:37:43 -0600

    feat(banner): change anchor to router-link

--- src/modules/layouts/components/MainBanner.vue
+++ src/modules/layouts/components/MainBanner.vue
@@ -14,7 +14,8 @@
     :loop="true"
   >
     <swiper-slide v-for="banner in banners.banners" :key="banner.id"
-      ><a :href="banner.redirect"><img :src="banner.url" alt="Imagen del banner" /></a
+      ><router-link :to="banner.redirect"
+        ><img :src="banner.url" alt="Imagen del banner" /></router-link
     ></swiper-slide>
   </swiper>
 </template>
