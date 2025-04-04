5c2b6f702b968656d7c8c7497d66614955ddf03a
EmanuelDDTech <emanuel@ddtech.mx>
2025-04-04 17:43:35 -0600

    feat(discountCode): add icon

---
+++ src/modules/common/icons/DiscountIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.5"
+  >
+    <path d="M9 15l6 -6"></path>
+    <circle cx="9.5" cy="9.5" r=".5" fill="currentColor"></circle>
+    <circle cx="14.5" cy="14.5" r=".5" fill="currentColor"></circle>
+    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+  </svg>
+</template>
