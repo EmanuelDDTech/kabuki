3905afa77eb6eb65733bd1e39764d11fd92e400b
EmanuelDDTech <emanuel@ddtech.mx>
2025-04-04 19:40:18 -0600

    feat(discountCode): create UpdateView

---
+++ src/modules/discountCode/pages/UpdateDiscountCode.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+import DiscountCodeForm from '../components/DiscountCodeForm.vue';
+
+const route = useRoute();
+const id: number = parseInt(route.params.id[0]);
+</script>
+
+<template>
+  <main class="mt-6 mx-4 p-4 sm:p-6 xl:p-8 bg-white shadow rounded-lg">
+    <DiscountCodeForm :id="id" />
+  </main>
+</template>
