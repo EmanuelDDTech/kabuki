11171d735df739efda60101af8fc540228ba9aaa
EmanuelDDTech <emanuel@ddtech.mx>
2025-04-04 19:40:02 -0600

    feat(discountCode): create CreateView

---
+++ src/modules/discountCode/pages/CreateDiscountCode.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import DiscountCodeForm from '../components/DiscountCodeForm.vue';
+</script>
+
+<template>
+  <main class="mt-6 mx-4 p-4 sm:p-6 xl:p-8 bg-white shadow rounded-lg">
+    <DiscountCodeForm />
+  </main>
+</template>
