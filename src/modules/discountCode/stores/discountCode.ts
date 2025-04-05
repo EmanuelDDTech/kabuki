a64ee940e63d1d554093d778be348ac98eaf6755
EmanuelDDTech <emanuel@ddtech.mx>
2025-04-04 19:38:58 -0600

    feat(discountCode): add delete and update on store

--- src/modules/discountCode/stores/discountCode.ts
+++ src/modules/discountCode/stores/discountCode.ts
@@ -1,11 +1,27 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
-import { type DiscountCode } from '../interfaces/discountCode.interface';
+import { DiscountType, type DiscountCode } from '../interfaces/discountCode.interface';
 import DiscountCodeAPI from '../api/DiscountCodeAPI';
+import { convertToISO, convertToYYYYMMDD } from '@/helpers/date';
+import CampaignAPI from '@/modules/campaign/api/CampaignAPI';
+
+const initialValues = {
+  id: 0,
+  code: '',
+  discount_type: DiscountType.FIXED,
+  discount_value: 0,
+  min_purchase: 0,
+  max_discount: 0,
+  usage_limit: 0,
+  times_used: 0,
+  expires_at: '',
+};
 
 export const useDiscountCodeStore = defineStore('discountCode', () => {
   const discountCodes = ref<DiscountCode[]>([]);
-  const discountCode = ref<DiscountCode | null>(null);
+  const discountCode = ref<DiscountCode>(initialValues);
+
+  const expires_at_ISO = ref('');
 
   const getDiscountCodesAll = async () => {
     try {
@@ -16,11 +32,87 @@ export const useDiscountCodeStore = defineStore('discountCode', () => {
     }
   };
 
+  const createDiscountCode = async () => {
+    setExpiresDate(expires_at_ISO.value);
+    const { id, ...discountCodeData } = discountCode.value;
+
+    try {
+      const { data } = await DiscountCodeAPI.create(discountCodeData);
+
+      return { msg: 'Código de descuento creado correctamente' };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  const setExpiresDate = (dateISO: string) => {
+    const formattedDate = convertToISO(dateISO);
+    discountCode.value.expires_at = formattedDate;
+  };
+
+  const cleanDiscountCode = () => {
+    discountCode.value = {
+      id: 0,
+      code: '',
+      discount_type: DiscountType.FIXED,
+      discount_value: 0,
+      min_purchase: 0,
+      max_discount: 0,
+      usage_limit: 0,
+      times_used: 0,
+      expires_at: '',
+    };
+
+    expires_at_ISO.value = '';
+  };
+
+  const deleteDiscountCode = async (id: number) => {
+    try {
+      await DiscountCodeAPI.delete(id);
+      await getDiscountCodesAll();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  const getDiscountCodeById = async (id: number) => {
+    try {
+      const { data } = await DiscountCodeAPI.getById(id);
+      discountCode.value = data;
+      expires_at_ISO.value = convertToYYYYMMDD(discountCode.value.expires_at);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
+  const UpdateDiscountCode = async () => {
+    setExpiresDate(expires_at_ISO.value);
+
+    const { id, ...discountCodeData } = discountCode.value;
+
+    try {
+      const { data } = await DiscountCodeAPI.update(id ?? 0, discountCodeData);
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  };
+
   return {
     discountCodes,
     discountCode,
+    expires_at_ISO,
 
     // Methods
     getDiscountCodesAll,
+    createDiscountCode,
+    cleanDiscountCode,
+    deleteDiscountCode,
+    getDiscountCodeById,
+    UpdateDiscountCode,
   };
 });
