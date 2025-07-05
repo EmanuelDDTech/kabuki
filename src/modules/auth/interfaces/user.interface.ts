95ed9862c2019c3619536b5ac912e8c7534a3546
EmanuelDDTech <emanuel@ddtech.mx>
2025-07-05 15:24:19 -0600

    feat(user.interface): create interface

--- src/modules/auth/interfaces/user.interface.ts
+++ src/modules/auth/interfaces/user.interface.ts
@@ -1,6 +1,10 @@
+import type { Cart } from '@/modules/cart/interfaces/cart.interface';
+
 export interface User {
   id: number;
   email: string;
   isAdmin: string;
   name: string;
+  verified: boolean;
+  carts: Cart[];
 }
