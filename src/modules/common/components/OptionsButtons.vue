04551df141d2a11b725bad02830e9ae7b4d2e9de
EmanuelDDTech <emanuel@ddtech.mx>
2025-07-14 23:07:44 -0600

    feat(OptionsButtons): add component for selecting options with dynamic buttons

---
+++ src/modules/common/components/OptionsButtons.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { Period } from '@/modules/cart/interfaces/sale.interface';
+
+interface Props {
+  options: Period[];
+  selectedOption: Period;
+}
+
+defineProps<Props>();
+
+defineEmits(['selectOption']);
+</script>
+<template>
+  <div class="mb-4 flex gap-2">
+    <button
+      v-for="option in options"
+      :key="option"
+      @click="$emit('selectOption', option)"
+      :class="[
+        'px-4 py-2 rounded',
+        selectedOption === option ? 'bg-blue-600 text-white' : 'bg-gray-200',
+      ]"
+    >
+      {{ option }}
+    </button>
+  </div>
+</template>
