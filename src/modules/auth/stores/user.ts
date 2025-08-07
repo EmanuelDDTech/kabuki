dead8af0e1076a0dae68e2f27b98322d5424d22a
EmanuelDDTech <emanuel@ddtech.mx>
2025-08-07 13:14:49 -0600

    feat(user store): create auth checking status functions

--- src/modules/auth/stores/user.ts
+++ src/modules/auth/stores/user.ts
@@ -2,9 +2,10 @@ import { ref, onMounted, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { useRouter } from 'vue-router';
 import AuthAPI from '../api/AuthAPI';
-import type { User } from '../interfaces';
+import { AuthStatus, type User } from '../interfaces';
 
 export const useUserStore = defineStore('user', () => {
+  const authStatus = ref<AuthStatus>(AuthStatus.Checking);
   const router = useRouter();
   const user = ref<User | null>();
 
@@ -23,8 +24,40 @@ export const useUserStore = defineStore('user', () => {
     router.push({ name: 'login' });
   }
 
+  const checkAuthStatus = async (): Promise<boolean> => {
+    try {
+      const { data } = await AuthAPI.auth();
+
+      if (!data) {
+        logout();
+        return false;
+      }
+
+      authStatus.value = AuthStatus.Authenticated;
+      user.value = data;
+    } catch (error) {
+      console.log(error);
+      logout();
+      return false;
+    }
+  };
+
   const getUserName = computed(() => (user.value?.name ? user.value?.name : ''));
   const isSet = computed(() => (user.value ? true : false));
 
-  return { user, getUserName, isSet, setUser, logout };
+  return {
+    user,
+
+    // Getters
+    getUserName,
+    isSet,
+    isAdmin: computed(() => user.value?.isAdmin),
+    isChecking: computed(() => authStatus.value === AuthStatus.Checking),
+    isAuthenticated: computed(() => authStatus.value === AuthStatus.Authenticated),
+
+    // Actions
+    setUser,
+    logout,
+    checkAuthStatus,
+  };
 });
