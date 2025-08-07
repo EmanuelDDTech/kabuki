b23598f8634216a94920a8c276590632fd962f52
EmanuelDDTech <emanuel@ddtech.mx>
2025-08-07 13:16:20 -0600

    feat(App.vue): add CardsLoader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import CardsLoader from './modules/common/components/CardsLoader.vue';
+import { useUserStore } from './modules/auth/stores/user';
+
+const userStore = useUserStore();
 </script>
 
 <template data-theme="light">
-  <RouterView />
+  <CardsLoader v-if="userStore.isChecking" />
+  <RouterView v-else />
 </template>
