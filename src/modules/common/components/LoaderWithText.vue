f0024d95c726a9d357c54f71fc0d8c9d61d608f6
EmanuelDDTech <emanuel@ddtech.mx>
2025-08-09 09:39:25 -0600

    feat(loaders): create new loaders

---
+++ src/modules/common/components/LoaderWithText.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import LoaderBouncingSquare from '@/modules/common/components/LoaderBouncingSquare.vue';
+
+interface Props {
+  text: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex justify-center items-center mt-6">
+    <p>{{ text }}</p>
+    <LoaderBouncingSquare class="inline-block ml-2 w-16 text-green-600" />
+  </div>
+</template>
